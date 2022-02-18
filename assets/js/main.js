192748dc7c41dc8fd180941dc4e0f7823eeefadf
Antônio Vinícius Menezes Medeiros <contato@antoniomedeiros.dev>
2022-02-17 22:39:09 -0300

    Bootstrap's tables are opt-in

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,8 @@ $(document).ready(function(){
         $('#fab button i').text(enterDesignMode ? "edit" : "visibility");
         document.designMode = enterDesignMode ? "on" : "off";
     });
+
+    // Bootstrap's tables are opt-in
+    // https://stackoverflow.com/a/39703599/1657502
+    $('table:not(.rouge-table)').addClass('table');
 });
